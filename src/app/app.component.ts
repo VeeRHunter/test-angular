fc3886641c5117d0e5c219dc6b73b3278251ca32
VeeR <veerhunter127@gmail.com>
2019-05-28 00:28:12 +0700

    Add Jqx menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { jqxMenuComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxmenu';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-angular';
+
+  @ViewChild('jqxMenu') jqxMenu: jqxMenuComponent;
+
+  private customeMenu = [
+    {
+      title: 'test 1',
+    },
+    {
+      title: 'test 2',
+    },
+    {
+      title: 'test 3',
+    },
+    {
+      title: 'test 4',
+    },
+    {
+      title: 'test 5',
+    }
+  ];
+
+  isRightClick(event: any): boolean {
+    let rightclick;
+    if (!event) { event = window.event; }
+    if (event.which) { rightclick = (event.which === 3); } else if (event.button) { rightclick = (event.button === 2); }
+    return rightclick;
+  }
+  contextmenuMenu(): boolean {
+    return false;
+  }
+  mousedown(event: any): boolean {
+    const rightClick = this.isRightClick(event) || jqx.mobile.isTouchDevice();
+    if (rightClick) {
+      // tslint:disable-next-line:radix
+      this.jqxMenu.open(parseInt(event.clientX), parseInt(event.clientY));
+      return false;
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { jqxGridComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxgrid';
+import { jqxInputComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxinput';
+import { jqxButtonComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxbuttons';
+import { jqxChartComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxchart';
+import { jqxMenuComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxmenu';
+import { jqxTreeGridComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxtreegrid';
+import { jqxCheckBoxComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxcheckbox';
+import { jqxRadioButtonComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxradiobutton';
+import { jqxSwitchButtonComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxswitchbutton';
+import { jqxTabsComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxtabs';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    jqxGridComponent, jqxInputComponent,
+    jqxButtonComponent, jqxChartComponent, jqxMenuComponent, jqxTreeGridComponent,
+    jqxCheckBoxComponent, jqxRadioButtonComponent, jqxSwitchButtonComponent,
+  ],
+  declarations: [AppComponent, jqxGridComponent, jqxInputComponent,
+    jqxButtonComponent, jqxChartComponent, jqxMenuComponent,
+    jqxCheckBoxComponent, jqxRadioButtonComponent, jqxSwitchButtonComponent,
+    jqxTreeGridComponent, jqxTabsComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
